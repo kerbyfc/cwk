f28e97269718de58b1f575d8e43ac6375ad5c3cb
kerbyfc <kerbyfc@gmail.com>
2013-10-18 18:17:23 +0400

    update

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject cwk "0.1.1"
+(defproject cwk "0.1.0-SNAPSHOT"
   :description "evals all source code from concrete inner-resource folder"
   :url "https://github.com/kerbyfc/cwk"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
+  :deploy-repositories [["internal" "/Users/kerbyfc/Dropbox/clojure_rep"]]
   :main cwk.core
   :profiles {:uberjar {:aot [cwk.core]}})
